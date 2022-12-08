a12baf5cd6ef8c077ae6a0070cd8708e1ef12251
Joan Ogando <jogando@neural.com.do>
2022-12-07 23:50:06 -0400

    fix

--- RangeKata/Program.fs
+++ RangeKata/Program.fs
@@ -5,11 +5,82 @@ open RangeClass
 
 [<EntryPoint>]
 let main argv =
-    let range = new RangeClass "[2,5)"
-    if range.contains [3; 4; 5] then printfn "Contains" else printfn "Does not contain"
-    let rangeTest = new RangeClass "[2,4]"
-    if range.overlapsRange rangeTest then printfn "overlaps" else printfn "does not overlap"
-    if range.equals rangeTest then printfn "equals" else printfn "not equal"
-    printfn "%A" range.getAllPoints 
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+
+    //tests 
+    
+    //contains
+    let mutable range = new RangeClass("[2,6)")
+    if range.contains [2;4] then printfn "contains" else printfn "does not contain"
+    if range.contains [-1; 1; 6; 10] then printfn "contains" else printfn "does not contain"
+
+    //getAllPoints
+    printfn "%A" range.getAllPoints
+
+    //containsRange
+    range <- new RangeClass("[2,5)")
+    let mutable testRange = new RangeClass("[7,10)")
+    if range.containsRange testRange then printfn "contains" else printfn "does not contain"
+
+    testRange <- new RangeClass("[3,10)")
+    if range.containsRange testRange then printfn "contains" else printfn "does not contain"
+
+    range <- new RangeClass("[3,5)")
+    testRange <- new RangeClass("[2,10)")
+    if range.containsRange testRange then printfn "contains" else printfn "does not contain"
+
+    range <- new RangeClass("[2,10)")
+    testRange <- new RangeClass("[3,5]")
+    if range.containsRange testRange then printfn "contains" else printfn "does not contain"
+
+    range <- new RangeClass("[3,5]")
+    testRange <- new RangeClass("[3,5]")
+    if range.containsRange testRange then printfn "contains" else printfn "does not contain"
+
+    //endPoints
+    range <- new RangeClass("[2,6)")
+    printfn "%A" range.endPoints
+
+    range <- new RangeClass("[2,6]")
+    printfn "%A" range.endPoints
+
+    range <- new RangeClass("(2,6)")
+    printfn "%A" range.endPoints
+
+    range <- new RangeClass("(2,6]")
+    printfn "%A" range.endPoints
+
+    //overlapsRange
+    range <- new RangeClass("[2,5)")
+    testRange <- new RangeClass("[7,10)")
+    if range.overlapsRange testRange then printfn "overlaps" else printfn "does not overlap"
+
+    range <- new RangeClass("[2,10)")
+    testRange <- new RangeClass("[3,5]")
+    if range.overlapsRange testRange then printfn "overlaps" else printfn "does not overlap"
+
+    range <- new RangeClass("[2,5)")
+    testRange <- new RangeClass("[3,10)")
+    if range.overlapsRange testRange then printfn "overlaps" else printfn "does not overlap"
+
+    range <- new RangeClass("[3,5)")
+    testRange <- new RangeClass("[2,10)")
+    if range.overlapsRange testRange then printfn "overlaps" else printfn "does not overlap"
+
+    //equals
+    range <- new RangeClass("[3,5)")
+    testRange <- new RangeClass("[3,5)")
+    if range.equals testRange then printfn "equals" else printfn "not equal"
+
+    range <- new RangeClass("[2,10)")
+    testRange <- new RangeClass("[3,5)")
+    if range.equals testRange then printfn "equals" else printfn "not equal"
+
+    range <- new RangeClass("[2,5)")
+    testRange <- new RangeClass("[3,10)")
+    if range.equals testRange then printfn "equals" else printfn "not equal"
+
+    range <- new RangeClass("[3,5)")
+    testRange <- new RangeClass("[2,10)")
+    if range.equals testRange then printfn "equals" else printfn "not equal"
+
+    0
--- RangeKata/Range.fs
+++ RangeKata/Range.fs
@@ -1,67 +1,62 @@
 ﻿module RangeClass
 
 open System.Text.RegularExpressions
-let rx = Regex(@"(\(|\[)\d+\,\d+(\)|\])", RegexOptions.Compiled)
 exception Error of string
+let rx = Regex(@"(\(|\[)\d+\,\d+(\)|\])", RegexOptions.Compiled)
 
 type RangeClass(range: string) =
-
-    let mutable startPoint = Unchecked.defaultof<int>
-    let mutable endPoint = Unchecked.defaultof<int>
+   
     let mutable rangeList = Unchecked.defaultof<List<int>>
-    do 
+
+    do
         try
             let matched = rx.IsMatch(range)
             if matched then 
-                if endPoint < startPoint then raise(Error("Range is not valid."))
+                let mutable rangeString = range
+                rangeString <- rangeString.Remove(0, 1)
+                rangeString <- rangeString.Remove(((String.length rangeString)-1), 1)
 
-                let splitted = Seq.toList range
+                let rangePoints = rangeString.Split ','
 
-                let mutable rangeStr = range
-                rangeStr <- rangeStr.Remove(0, 1)
-                rangeStr <- rangeStr.Remove(((String.length rangeStr) - 1), 1)
+                let mutable startP = rangePoints[0] |> string |> int
+                let mutable endP = rangePoints[1] |> string |> int
+           
+                if range[0] = '(' then startP <- startP+1 
+                if range[((String.length range)-1)] = ')' then endP <- endP-1 
 
-                let rangesPoints = rangeStr.Split ','
-
-                let startP = rangesPoints[0] |> string |> int
-                let endP = rangesPoints[1] |> string |> int
-
-                if splitted[0] = '[' then 
-                    startPoint <- startP
-                else startPoint <- startP+1 
-                if splitted[(List.length splitted)-1] = ']' then 
-                    endPoint <- endP
-                else endPoint <- endP-1
-            
-                rangeList <- [startPoint..endPoint]
+                rangeList <- [startP..endP]
             else raise(Error("Range is not valid"))
         with
-            | Error(str) -> printfn "%s" str
+        | Error(str) -> printfn "%s" str
+
 
     member this.contains(numList: List<int>) : bool =
         let mutable result = true
+
         let sortedList = List.sort numList
-        
+
         if sortedList[0] < rangeList[0] then result <- false
         if sortedList[(List.length sortedList)-1] > rangeList[(List.length rangeList)-1] then result <- false
 
         result
 
-    member this.getAllPoints =
+    member this.getAllPoints : List<int> = 
         rangeList
 
-    member this.containsRange(range: RangeClass) = 
+    member this.containsRange(range: RangeClass) : bool =
         this.contains(range.getAllPoints)
 
-    member this.endPoints =
+    member this.endPoints : List<int> =
         [rangeList[0]; rangeList[(List.length rangeList)-1]]
 
-    member this.overlapsRange(range: RangeClass) =
+    member this.overlapsRange(range: RangeClass) : bool =        
         let pRangeList = range.getAllPoints
-        if(pRangeList[0] < rangeList[0] || pRangeList[(List.length pRangeList)-1] > rangeList[(List.length rangeList)-1]) 
-        then false
-        else true
+        let mutable result = true
 
-    member this.equals(range: RangeClass) =
-        if range.getAllPoints = rangeList then true
-        else false
+        if pRangeList[(List.length pRangeList)-1] < rangeList[0] then result <- false
+        if rangeList[(List.length rangeList)-1] < pRangeList[0] then result <- false
+
+        result
+
+    member this.equals(range: RangeClass) : bool =
+        range.getAllPoints = rangeList
